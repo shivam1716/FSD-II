4059fc9945b0d9a968eadc62dcd922677899842e
Shivam Singh <singhshivam.ss.2101@gmail.com>
2026-07-17 14:03:11 +0530

    var and let

---
+++ datatypes.js
@@ -0,0 +1,37 @@
+//string
+
+let age="age is 25";
+console.log(age);
+console.log(typeof(age));
+
+
+//boolean
+
+let s=true
+console.log(s);
+console.log(typeof(s));
+
+
+//null
+
+let s=null;
+console.log(s);
+console.log(typeof(s));
+
+
+//bigint
+
+let id=32313453263477n;
+console.log(id);
+console.log(typeof(id));
+
+
+//symbol
+
+let id= Symbol("id");
+console.log(id);
+console.log(typeof(id));
+
+
+
+
---
+++ var-let.js
@@ -0,0 +1,12 @@
+let students={
+    id:101,
+    name:"amit",
+    marks:85
+}
+
+console.log( students);
+
+//hoisting
+
+let a=10;
+console.log(a);
